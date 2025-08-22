279abe458020a821d0f0445259c478b896c6cfe1
sdutson <sdutson222@gmail.com>
2025-08-22 12:54:58 -0600

    Added Edit Window)

--- Projects/LLC_Files/chop-chop-main/src/main.rs
+++ Projects/LLC_Files/chop-chop-main/src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Box as GtkBox, Button, Orientation}; // TODO: Remove unneeded use statements. 
+use gtk::{Application, ApplicationWindow, Box as GtkBox, Button, Orientation, Entry}; // TODO: Remove unneeded use statements. 
 const APP_ID: &str = "org.gtk_rs.HelloWorld3";
 
 fn main() {
@@ -14,10 +14,31 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
-
     // Build HStack to store Menu buttons.
     let top_menu_hsk = GtkBox::new(Orientation::Horizontal, 10);
+    build_menu_bar(&top_menu_hsk);
 
+    // Build VStack for 'edit' window. 
+    let edit_window_vsk = GtkBox::new(Orientation::Vertical, 10);
+    build_edit_window(&edit_window_vsk);
+
+    // Build 'parent' HStack.
+    let parent_hsk = GtkBox::new(Orientation::Vertical, 10);
+    parent_hsk.append(&top_menu_hsk);
+    parent_hsk.append(&edit_window_vsk);
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&parent_hsk)
+        .build();
+
+    // Present window
+    window.present();
+}
+
+fn build_menu_bar(top_menu_hsk: &GtkBox) {
     // Create Menu Buttons.
     let materials_menu_button = Button::builder()
     .label("Materials")
@@ -42,7 +63,6 @@ fn build_ui(app: &Application) {
     let top_menu_spacer_right = GtkBox::new(Orientation::Horizontal, 0);
     top_menu_spacer_right.set_width_request(50);
 
-
     // Build top menu hstack. 
     top_menu_hsk.append(&top_menu_spacer_right);
     top_menu_hsk.append(&materials_menu_button);
@@ -50,14 +70,38 @@ fn build_ui(app: &Application) {
     top_menu_hsk.append(&solver_menu_button);
     top_menu_hsk.append(&top_menu_spacer_left);
     top_menu_hsk.append(&exit_button);
+}
 
-    // Create a window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My GTK App")
-        .child(&top_menu_hsk)
-        .build();
+fn build_edit_window(edit_window_vsk: &GtkBox) {
+    // Build input sections. 
+    let description_input_section = GtkBox::new(Orientation::Vertical, 10);
+    build_input_section(&description_input_section, &String::from("Description"));
 
-    // Present window
-    window.present();
+    let quantity_input_section = GtkBox::new(Orientation::Vertical, 10);
+    build_input_section(&quantity_input_section, &String::from("Quantity"));
+
+    let length_input_section = GtkBox::new(Orientation::Vertical, 10);
+    build_input_section(&length_input_section, &String::from("Length"));
+
+    let price_input_section = GtkBox::new(Orientation::Vertical, 10);
+    build_input_section(&price_input_section, &String::from("Price"));
+
+    // Build 'add' button. 
+    let add_button = Button::builder()
+    .label("Add")
+    .build();
+
+    // Build edit window. 
+    edit_window_vsk.append(&description_input_section);
+    edit_window_vsk.append(&quantity_input_section);
+    edit_window_vsk.append(&length_input_section);
+    edit_window_vsk.append(&price_input_section);
+    edit_window_vsk.append(&add_button);
+}
+
+fn build_input_section(vsk: &GtkBox, title: &String) { // TODO: Use custom enum to allow for dynamic 'child' passing. 
+    let entry = Entry::new();
+    entry.set_placeholder_text(Some(title));
+
+    vsk.append(&entry);
 }
