4552047b7daf75d60b2f015b8f94207359145180
Matthew Dutson <mattdutson@icloud.com>
2025-08-22 19:38:01 -0600

    Set up basic abstractions

---
+++ src/modeling.rs
@@ -0,0 +1,57 @@
+/// A type of material
+///
+/// Examples:
+/// - Pine 2x4
+/// - Green yarn
+#[derive(Clone, Debug, PartialEq)]
+pub struct Substance {
+    pub name: String,
+}
+
+/// A substance available at a specific dimension
+///
+/// Assumed to be 1D for now
+///
+/// Examples:
+/// - 8-foot pine 2x4
+/// - 100-meter spoon of green yarn
+#[derive(Clone, Debug)]
+pub struct Material {
+    pub substance: Substance,
+    pub length: f32,
+}
+
+/// Represents an available supply of a material
+///
+/// A price of zero indicates that the material is on-hand (free)
+///
+/// A max_quantity of zero indicates that the supply is unlimited
+///
+/// Examples:
+/// - There are five 8-foot pine 2x4s on-hand (price $0, max_quantity 5)
+/// - 100-meter spools of yarn are purchasable for $100 each (price $100, max_quantity 0)
+#[derive(Debug)]
+pub struct Supply {
+    pub material: Material,
+    pub price: f32,
+    pub max_quantity: u32,
+}
+
+/// Represents a desired part
+///
+/// Assumed 1D for now
+#[derive(Debug)]
+pub struct Part {
+    pub substance: Substance,
+    pub length: f32,
+    pub quantity: u32,
+}
+
+/// Represents a set of of cuts to perform on an object (instance of a material)
+///
+/// Assumed 1D for now
+#[derive(Debug)]
+pub struct CutList {
+    pub material: Material,
+    pub cuts: Vec<f32>,
+}
---
+++ src/solvers.rs
@@ -0,0 +1,15 @@
+pub mod naive_solver;
+
+use crate::modeling::{CutList, Part, Supply};
+
+pub trait Solver {
+    fn solve(&self, supplies: &[&Supply], parts: &[&Part]) -> Result<Solution, String>;
+}
+
+#[derive(Debug)]
+#[allow(dead_code)] // TODO: Temporary
+pub struct Solution {
+    cut_lists: Vec<CutList>,
+    supply_consumption: Vec<u32>,
+    total_price: f32,
+}
