ffcd913ca08bf0a1d7e72b753d4f496b9e6475d5
sdutson <sdutson222@gmail.com>
2025-09-29 07:45:48 -0600

    cleared old solver code

--- src/solvers/oned_solver.rs
+++ src/solvers/oned_solver.rs
@@ -20,52 +20,6 @@ impl Solver for OneDSolver {
         progress_sender: Option<Sender<f64>>,
         result_sender: Option<Sender<Result<Solution, String>>>,
     ) -> Result<Solution, String> {
-        // Break up supplies and parts by material type.
-        let mut supplies_by_material: HashMap<String, Vec<Supply>> = HashMap::new();
-        let mut parts_by_material: HashMap<String, Vec<Part>> = HashMap::new();
-
-        for supply in supplies {
-            let material = &supply.material;
-            supplies_by_material
-                .entry(material.clone()) // clone the key if needed
-                .or_insert_with(Vec::new) // insert empty vector if key doesn't exist
-                .push(supply.clone());
-        }
-        for part in parts {
-            let material = &part.material;
-            parts_by_material
-                .entry(material.clone())
-                .or_insert_with(Vec::new)
-                .push(part.clone());
-        }
-
-        for material in parts_by_material.keys() {
-            // Make list of parts needed for the current material.
-            let mut cuts_set: Vec<Part> = Vec::new();
-            if let Some(parts) = parts_by_material.get(material) {
-                for part in parts {
-                    cuts_set.push(part.clone());
-                }
-            }
-            // Make list of supplies available for current material.
-            let mut supply_set: Vec<Supply> = Vec::new();
-            if let Some(supplies) = supplies_by_material.get(material) {
-                for supply in supplies {
-                    supply_set.push(supply.clone());
-                }
-            }
-            // TODO:
-        }
-
-        // TODO: Convert output into cut list.
-
-        // TODO: Change to return actual value.
-        let solution = Ok(Solution {
-            cut_lists,
-            supply_consumption,
-            total_price,
-        });
-        self.send_final_result(solution.clone(), progress_sender, result_sender);
-        solution
+       
     }
 }
